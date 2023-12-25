9357724fe8660eb52a029cfd6ec9f8e8b5a112e2
teatw001 <duydhph26793@fpt.edu.vn>
2023-12-25 19:41:38 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://stcinemas.id.vn/api/film");
+        const data = await response.json();
+
+        // Log data to the console
+        console.log("Film Data:", data);
+      } catch (error) {
+        console.error("Error fetching film data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // The empty dependency array ensures this effect runs only once on component mount
+
   return <>Hello Baby</>;
 }
 

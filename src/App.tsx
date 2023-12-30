eb3063b75de67aa8e6d07b7fae290c4044fcce33
teatw001 <duydhph26793@fpt.edu.vn>
2023-12-31 00:14:58 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,21 @@ import Ticket from "./pages/Clients/TICKET/Ticket";
 import Movies from "./pages/Clients/Movies/Movies";
 // import F_B from "./pages/Clients/F&B/F&B";
 import Login from "./pages/Clients/Login/Login";
-import LayoutAdmin from "./Layout/LayoutAdmin/LayoutAdmin";
-import ListFilm from "./pages/Admin/ListFilm/ListFilm";
-import { useEffect } from "react";
-import ListCate from "./pages/Admin/Category/ListCategory";
-import ListCinema from "./pages/Admin/Cinemas/ListCinema";
-import ListShow from "./pages/Admin/Time_Detail/ListShow";
-import ListBookTicket from "./pages/Admin/Book-Ticket/ListBookTicket";
-import ListMovieRoom from "./pages/Admin/MovieRoom/ListMovieRoom";
-import ListFood from "./pages/Admin/Food/ListFood";
-import ListCateDetail from "./pages/Admin/CateDetail/ListCateDetail";
+// import LayoutAdmin from "./Layout/LayoutAdmin/LayoutAdmin";
+// import ListFilm from "./pages/Admin/ListFilm/ListFilm";
+// import { useEffect } from "react";
+// import ListCate from "./pages/Admin/Category/ListCategory";
+// import ListCinema from "./pages/Admin/Cinemas/ListCinema";
+// import ListShow from "./pages/Admin/Time_Detail/ListShow";
+// import ListBookTicket from "./pages/Admin/Book-Ticket/ListBookTicket";
+// import ListMovieRoom from "./pages/Admin/MovieRoom/ListMovieRoom";
+// import ListFood from "./pages/Admin/Food/ListFood";
+// import ListCateDetail from "./pages/Admin/CateDetail/ListCateDetail";
 
-import ListTime from "./pages/Admin/time/listTime";
+// import ListTime from "./pages/Admin/time/listTime";
 
-import { updateToken, setUserId } from "./components/CinemaSlice/authSlice";
-import { useDispatch } from "react-redux";
+// import { updateToken, setUserId } from "./components/CinemaSlice/authSlice";
+// import { useDispatch } from "react-redux";
 import Payment from "./pages/Clients/Payment/Payment";
 
 import ChoosePop from "./pages/Clients/ChoosePop/ChoosePop";
@@ -31,37 +31,37 @@ import TicketBookingDetails from "./pages/Clients/Ticket-booking-details/TicketB
 
 import PaymentMomo from "./pages/Clients/Payment/PaymentMomo";
 
-import ListVouchers from "./pages/Admin/Vouchers/ListVouchers";
-import ForgotPassword from "./pages/Clients/Forgot-password";
-import ResetPassword from "./pages/Clients/Reset-password/reset-password";
-import NotFound from "./pages/Clients/NotFoundPages/NotFound";
+// import ListVouchers from "./pages/Admin/Vouchers/ListVouchers";
+// import ForgotPassword from "./pages/Clients/Forgot-password";
+// import ResetPassword from "./pages/Clients/Reset-password/reset-password";
+// import NotFound from "./pages/Clients/NotFoundPages/NotFound";
 import LayoutProfile from "./Layout/LayoutUser/LayoutProfile";
 import Profile from "./pages/Clients/Profile/Profile";
 import BookTicketUser from "./pages/Clients/BookTicketUser/BookTicketUser";
-import ListUser from "./pages/Admin/User/ListUser";
+// import ListUser from "./pages/Admin/User/ListUser";
 
 import MemberInfo from "./pages/Clients/member-info/member-info";
-import MemberInfoAdmin from "./pages/Admin/Members/Members";
+// import MemberInfoAdmin from "./pages/Admin/Members/Members";
 import ResultSuccess from "./pages/Clients/Result/ResultSuccess";
 // import ListBlog from "./pages/Admin/Blogs/ListBlog";
 // import BlogsDetail from "./pages/Clients/Blogs-Detail/BlogsDetail";
-import Dashbroad from "./pages/Admin/Dashbroad/Dashbroad";
+// import Dashbroad from "./pages/Admin/Dashbroad/Dashbroad";
 import ResultPaymentCoin from "./components/Clients/ResultPaymentCoin/ResultPaymentCoin";
-import Dashbroad_Admin_Cinema from "./pages/Admin/Dashbroad/Dashboard-Admin_Cinema";
-import Dashboard_Staff from "./pages/Admin/Dashbroad/Dashboard-Staff";
+// import Dashbroad_Admin_Cinema from "./pages/Admin/Dashbroad/Dashboard-Admin_Cinema";
+// import Dashboard_Staff from "./pages/Admin/Dashbroad/Dashboard-Staff";
 
 function App() {
-  const getuserId = localStorage.getItem("user");
-  const userId = JSON.parse(`${getuserId}`);
+  // const getuserId = localStorage.getItem("user");
+  // const userId = JSON.parse(`${getuserId}`);
 
-  const dashbroadElement =
-    userId?.role == 1 ? (
-      <Dashbroad />
-    ) : userId?.role == 2 ? (
-      <Dashbroad_Admin_Cinema />
-    ) : (
-      <Dashboard_Staff />
-    );
+  // const dashbroadElement =
+  //   userId?.role == 1 ? (
+  //     <Dashbroad />
+  //   ) : userId?.role == 2 ? (
+  //     <Dashbroad_Admin_Cinema />
+  //   ) : (
+  //     <Dashboard_Staff />
+  //   );
 
   const router = createBrowserRouter([
     {
@@ -140,86 +140,86 @@ function App() {
         },
       ],
     },
-    {
-      path: "/forgot-password",
-      element: <ForgotPassword />,
-    },
-    {
-      path: "/reset-password",
-      element: <ResetPassword />,
-    },
-    {
-      path: "/admin",
-      element: <LayoutAdmin />,
+    // {
+    //   path: "/forgot-password",
+    //   element: <ForgotPassword />,
+    // },
+    // {
+    //   path: "/reset-password",
+    //   element: <ResetPassword />,
+    // },
+    // {
+    //   path: "/admin",
+    //   element: <LayoutAdmin />,
 
-      children: [
-        {
-          path: "/admin/listfilm",
-          element: <ListFilm />,
-        },
-        {
-          path: "members",
-          element: <MemberInfoAdmin />,
-        },
-        {
-          path: "/admin/vouchers",
-          element: <ListVouchers />,
-        },
-        {
-          index: true,
-          element: dashbroadElement,
-        },
-        {
-          path: "/admin/dashboards/:cinemaId",
-          element: <Dashbroad_Admin_Cinema />,
-        },
-        {
-          path: "/admin/staff",
-          element: <Dashboard_Staff />,
-        },
-        {
-          path: "/admin/book_ticket",
-          element: <ListBookTicket />,
-        },
-        {
-          path: "/admin/listcate",
-          element: <ListCate />,
-        },
-        {
-          path: "/admin/time",
-          element: <ListTime />,
-        },
-        {
-          path: "/admin/cinema",
-          element: <ListCinema />,
-        },
+    //   children: [
+    //     {
+    //       path: "/admin/listfilm",
+    //       element: <ListFilm />,
+    //     },
+    //     {
+    //       path: "members",
+    //       element: <MemberInfoAdmin />,
+    //     },
+    //     {
+    //       path: "/admin/vouchers",
+    //       element: <ListVouchers />,
+    //     },
+    //     {
+    //       index: true,
+    //       element: dashbroadElement,
+    //     },
+    //     {
+    //       path: "/admin/dashboards/:cinemaId",
+    //       element: <Dashbroad_Admin_Cinema />,
+    //     },
+    //     {
+    //       path: "/admin/staff",
+    //       element: <Dashboard_Staff />,
+    //     },
+    //     {
+    //       path: "/admin/book_ticket",
+    //       element: <ListBookTicket />,
+    //     },
+    //     {
+    //       path: "/admin/listcate",
+    //       element: <ListCate />,
+    //     },
+    //     {
+    //       path: "/admin/time",
+    //       element: <ListTime />,
+    //     },
+    //     {
+    //       path: "/admin/cinema",
+    //       element: <ListCinema />,
+    //     },
 
-        {
-          path: "/admin/show",
-          element: <ListShow />,
-        },
-        {
-          path: "/admin/movieroom",
-          element: <ListMovieRoom />,
-        },
-        {
-          path: "/admin/food",
-          element: <ListFood />,
-        },
-        {
-          path: "/admin/category_detail",
-          element: <ListCateDetail />,
-        },
-        {
-          path: "/admin/user",
-          element: <ListUser />,
-        },
-        // {
-        //   path: "/admin/blogs",
-        //   element: <ListBlog />,
-        // },
-      ],
-    },
+    //     {
+    //       path: "/admin/show",
+    //       element: <ListShow />,
+    //     },
+    //     {
+    //       path: "/admin/movieroom",
+    //       element: <ListMovieRoom />,
+    //     },
+    //     {
+    //       path: "/admin/food",
+    //       element: <ListFood />,
+    //     },
+    //     {
+    //       path: "/admin/category_detail",
+    //       element: <ListCateDetail />,
+    //     },
+    //     {
+    //       path: "/admin/user",
+    //       element: <ListUser />,
+    //     },
+    //     // {
+    //     //   path: "/admin/blogs",
+    //     //   element: <ListBlog />,
+    //     // },
+    //   ],
+    // },
     {
       path: "/login",
       element: <Login />,
@@ -236,39 +236,39 @@ function App() {
       path: "/ResultNapTien/:id",
       element: <ResultSuccess />,
     },
-    {
-      path: "/*",
-      element: <NotFound />,
-    },
+    // {
+    //   path: "/*",
+    //   element: <NotFound />,
+    // },
   ]);
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    const timeoutDuration = 1000 * 60 * 15; // 10 seconds
+  // useEffect(() => {
+  //   const timeoutDuration = 1000 * 60 * 15; // 10 seconds
 
-    const timeoutCallback = async () => {
-      try {
-        // Dispatch the actions and wait for them to complete
-        await Promise.all([
-          dispatch(updateToken(null)),
-          dispatch(setUserId(null)),
-          localStorage.clear(),
-        ]);
+  //   const timeoutCallback = async () => {
+  //     try {
+  //       // Dispatch the actions and wait for them to complete
+  //       await Promise.all([
+  //         dispatch(updateToken(null)),
+  //         dispatch(setUserId(null)),
+  //         localStorage.clear(),
+  //       ]);
 
-        // Once both actions are completed, navigate to the root path
-        window.location.href = "/";
-      } catch (error) {
-        // Handle any potential errors
-        console.error("Error during dispatch:", error);
-      }
-    };
+  //       // Once both actions are completed, navigate to the root path
+  //       window.location.href = "/";
+  //     } catch (error) {
+  //       // Handle any potential errors
+  //       console.error("Error during dispatch:", error);
+  //     }
+  //   };
 
-    const timeoutId = setInterval(timeoutCallback, timeoutDuration);
+  //   const timeoutId = setInterval(timeoutCallback, timeoutDuration);
 
-    return () => {
-      clearInterval(timeoutId);
-    };
-  }, [dispatch]);
+  //   return () => {
+  //     clearInterval(timeoutId);
+  //   };
+  // }, [dispatch]);
 
   return <RouterProvider router={router} />;
 }

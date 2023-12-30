d05e2bace5d0ab453e0b13d31dd7b5ef6e349246
teatw001 <duydhph26793@fpt.edu.vn>
2023-12-31 00:19:24 +0700

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LayoutUser from "./Layout/LayoutUser/LayoutUser";
 import HomePages from "./pages/Clients/Homepages/home";
-import BookingSeat from "./pages/Clients/TICKET - SEAT LAYOUT/seat";
+// import BookingSeat from "./pages/Clients/TICKET - SEAT LAYOUT/seat";
 // import Cinema from "./pages/Clients/Cinema/Cinema";
 // import Orther from "./pages/Clients/Orther/Orther";
-import Movie_About from "./pages/Clients/MOVIE-ABOUT/Movie-About";
-import Ticket from "./pages/Clients/TICKET/Ticket";
-import Movies from "./pages/Clients/Movies/Movies";
+// import Movie_About from "./pages/Clients/MOVIE-ABOUT/Movie-About";
+// import Ticket from "./pages/Clients/TICKET/Ticket";
+// import Movies from "./pages/Clients/Movies/Movies";
 // import F_B from "./pages/Clients/F&B/F&B";
 // import Login from "./pages/Clients/Login/Login";
 // import LayoutAdmin from "./Layout/LayoutAdmin/LayoutAdmin";
@@ -72,22 +72,22 @@ function App() {
           path: "/",
           element: <HomePages />,
         },
-        {
-          path: "/book-ticket/:id",
-          element: <BookingSeat />,
-        },
-        {
-          path: "/movie_about/:id",
-          element: <Movie_About />,
-        },
-        {
-          path: "/ticket",
-          element: <Ticket />,
-        },
-        {
-          path: "/movies",
-          element: <Movies />,
-        },
+        // {
+        //   path: "/book-ticket/:id",
+        //   element: <BookingSeat />,
+        // },
+        // {
+        //   path: "/movie_about/:id",
+        //   element: <Movie_About />,
+        // },
+        // {
+        //   path: "/ticket",
+        //   element: <Ticket />,
+        // },
+        // {
+        //   path: "/movies",
+        //   element: <Movies />,
+        // },
         // {
         //   path: "/resultpaymentcoins",
         //   element: <ResultPaymentCoin />,

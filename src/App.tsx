d5d9e192a4ca48ed893583362f616f77ce98570d
teatw001 <duydhph26793@fpt.edu.vn>
2023-12-31 00:22:21 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ import HomePages from "./pages/Clients/Homepages/home";
 // import { useDispatch } from "react-redux";
 // import Payment from "./pages/Clients/Payment/Payment";
 
-import ChoosePop from "./pages/Clients/ChoosePop/ChoosePop";
+// import ChoosePop from "./pages/Clients/ChoosePop/ChoosePop";
 // import TicketBookingDetails from "./pages/Clients/Ticket-booking-details/TicketBookingDetails";
 
 // import PaymentMomo from "./pages/Clients/Payment/PaymentMomo";
@@ -50,19 +50,229 @@ import ChoosePop from "./pages/Clients/ChoosePop/ChoosePop";
 // import Dashbroad_Admin_Cinema from "./pages/Admin/Dashbroad/Dashboard-Admin_Cinema";
 // import Dashboard_Staff from "./pages/Admin/Dashbroad/Dashboard-Staff";
 
+// function App() {
+//   // const getuserId = localStorage.getItem("user");
+//   // const userId = JSON.parse(`${getuserId}`);
+
+//   // const dashbroadElement =
+//   //   userId?.role == 1 ? (
+//   //     <Dashbroad />
+//   //   ) : userId?.role == 2 ? (
+//   //     <Dashbroad_Admin_Cinema />
+//   //   ) : (
+//   //     <Dashboard_Staff />
+//   //   );
+
+//   const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <LayoutUser />,
+//       children: [
+//         {
+//           path: "/",
+//           element: <HomePages />,
+//         },
+//         // {
+//         //   path: "/book-ticket/:id",
+//         //   element: <BookingSeat />,
+//         // },
+//         // {
+//         //   path: "/movie_about/:id",
+//         //   element: <Movie_About />,
+//         // },
+//         // {
+//         //   path: "/ticket",
+//         //   element: <Ticket />,
+//         // },
+//         // {
+//         //   path: "/movies",
+//         //   element: <Movies />,
+//         // },
+//         // {
+//         //   path: "/resultpaymentcoins",
+//         //   element: <ResultPaymentCoin />,
+//         // },
+//         // {
+//         //   path: "/F&B",
+//         //   element: <F_B />,
+//         // },
+//         // {
+//         //   path: "/cinema",
+//         //   element: <Cinema />,
+//         // },
+//         // {
+//         //   path: "/orther",
+//         //   element: <Orther />,
+//         // },
+//         {
+//           path: "/choosepop",
+//           element: <ChoosePop />,
+//         },
+//         // {
+//         //   path: "/Tiketbookingdetail",
+//         //   element: <TicketBookingDetails />,
+//         // },
+//         // {
+//         //   path: "/blog/:id",
+//         //   element: <BlogsDetail />,
+//         // },
+//         // {
+//         //   path: "/info_account",
+//         //   element: <LayoutProfile />,
+//         //   children: [
+//         //     {
+//         //       path: "", // Đường dẫn mặc định khi truy cập /info_account
+//         //       element: <Profile />,
+//         //     },
+//         //     {
+//         //       path: "profile", // Đường dẫn mặc định khi truy cập /info_account
+//         //       element: <Profile />,
+//         //     },
+//         //     {
+//         //       path: "BookTicketUser",
+//         //       element: <BookTicketUser />,
+//         //     },
+//         //     {
+//         //       path: "member-info",
+//         //       element: <MemberInfo />,
+//         //     },
+//         //   ],
+//         // },
+//       ],
+//     },
+//     // {
+//     //   path: "/forgot-password",
+//     //   element: <ForgotPassword />,
+//     // },
+//     // {
+//     //   path: "/reset-password",
+//     //   element: <ResetPassword />,
+//     // },
+//     // {
+//     //   path: "/admin",
+//     //   element: <LayoutAdmin />,
+
+//     //   children: [
+//     //     {
+//     //       path: "/admin/listfilm",
+//     //       element: <ListFilm />,
+//     //     },
+//     //     {
+//     //       path: "members",
+//     //       element: <MemberInfoAdmin />,
+//     //     },
+//     //     {
+//     //       path: "/admin/vouchers",
+//     //       element: <ListVouchers />,
+//     //     },
+//     //     {
+//     //       index: true,
+//     //       element: dashbroadElement,
+//     //     },
+//     //     {
+//     //       path: "/admin/dashboards/:cinemaId",
+//     //       element: <Dashbroad_Admin_Cinema />,
+//     //     },
+//     //     {
+//     //       path: "/admin/staff",
+//     //       element: <Dashboard_Staff />,
+//     //     },
+//     //     {
+//     //       path: "/admin/book_ticket",
+//     //       element: <ListBookTicket />,
+//     //     },
+//     //     {
+//     //       path: "/admin/listcate",
+//     //       element: <ListCate />,
+//     //     },
+//     //     {
+//     //       path: "/admin/time",
+//     //       element: <ListTime />,
+//     //     },
+//     //     {
+//     //       path: "/admin/cinema",
+//     //       element: <ListCinema />,
+//     //     },
+
+//     //     {
+//     //       path: "/admin/show",
+//     //       element: <ListShow />,
+//     //     },
+//     //     {
+//     //       path: "/admin/movieroom",
+//     //       element: <ListMovieRoom />,
+//     //     },
+//     //     {
+//     //       path: "/admin/food",
+//     //       element: <ListFood />,
+//     //     },
+//     //     {
+//     //       path: "/admin/category_detail",
+//     //       element: <ListCateDetail />,
+//     //     },
+//     //     {
+//     //       path: "/admin/user",
+//     //       element: <ListUser />,
+//     //     },
+//     //     // {
+//     //     //   path: "/admin/blogs",
+//     //     //   element: <ListBlog />,
+//     //     // },
+//     //   ],
+//     // },
+//     // {
+//     //   path: "/login",
+//     //   element: <Login />,
+//     // },
+//     // {
+//     //   path: "/payment/:id_code",
+//     //   element: <Payment />,
+//     // },
+//     // {
+//     //   path: "/PayMentMoMo/:id_code",
+//     //   element: <PaymentMomo />,
+//     // },
+//     // {
+//     //   path: "/ResultNapTien/:id",
+//     //   element: <ResultSuccess />,
+//     // },
+//     // {
+//     //   path: "/*",
+//     //   element: <NotFound />,
+//     // },
+//   ]);
+//   // const dispatch = useDispatch();
+
+//   // useEffect(() => {
+//   //   const timeoutDuration = 1000 * 60 * 15; // 10 seconds
+
+//   //   const timeoutCallback = async () => {
+//   //     try {
+//   //       // Dispatch the actions and wait for them to complete
+//   //       await Promise.all([
+//   //         dispatch(updateToken(null)),
+//   //         dispatch(setUserId(null)),
+//   //         localStorage.clear(),
+//   //       ]);
+
+//   //       // Once both actions are completed, navigate to the root path
+//   //       window.location.href = "/";
+//   //     } catch (error) {
+//   //       // Handle any potential errors
+//   //       console.error("Error during dispatch:", error);
+//   //     }
+//   //   };
+
+//   //   const timeoutId = setInterval(timeoutCallback, timeoutDuration);
+
+//   //   return () => {
+//   //     clearInterval(timeoutId);
+//   //   };
+//   // }, [dispatch]);
+
+//   return <RouterProvider router={router} />;
+// }
 function App() {
-  // const getuserId = localStorage.getItem("user");
-  // const userId = JSON.parse(`${getuserId}`);
-
-  // const dashbroadElement =
-  //   userId?.role == 1 ? (
-  //     <Dashbroad />
-  //   ) : userId?.role == 2 ? (
-  //     <Dashbroad_Admin_Cinema />
-  //   ) : (
-  //     <Dashboard_Staff />
-  //   );
-
   const router = createBrowserRouter([
     {
       path: "/",
@@ -72,204 +282,14 @@ function App() {
           path: "/",
           element: <HomePages />,
         },
+
         // {
-        //   path: "/book-ticket/:id",
-        //   element: <BookingSeat />,
-        // },
-        // {
-        //   path: "/movie_about/:id",
-        //   element: <Movie_About />,
-        // },
-        // {
-        //   path: "/ticket",
-        //   element: <Ticket />,
-        // },
-        // {
-        //   path: "/movies",
-        //   element: <Movies />,
-        // },
-        // {
-        //   path: "/resultpaymentcoins",
-        //   element: <ResultPaymentCoin />,
-        // },
-        // {
-        //   path: "/F&B",
-        //   element: <F_B />,
-        // },
-        // {
-        //   path: "/cinema",
-        //   element: <Cinema />,
-        // },
-        // {
-        //   path: "/orther",
-        //   element: <Orther />,
-        // },
-        {
-          path: "/choosepop",
-          element: <ChoosePop />,
-        },
-        // {
-        //   path: "/Tiketbookingdetail",
-        //   element: <TicketBookingDetails />,
-        // },
-        // {
-        //   path: "/blog/:id",
-        //   element: <BlogsDetail />,
-        // },
-        // {
-        //   path: "/info_account",
-        //   element: <LayoutProfile />,
-        //   children: [
-        //     {
-        //       path: "", // Đường dẫn mặc định khi truy cập /info_account
-        //       element: <Profile />,
-        //     },
-        //     {
-        //       path: "profile", // Đường dẫn mặc định khi truy cập /info_account
-        //       element: <Profile />,
-        //     },
-        //     {
-        //       path: "BookTicketUser",
-        //       element: <BookTicketUser />,
-        //     },
-        //     {
-        //       path: "member-info",
-        //       element: <MemberInfo />,
-        //     },
-        //   ],
+        //   path: "/search",
+        //   element: <Search />,
         // },
       ],
     },
-    // {
-    //   path: "/forgot-password",
-    //   element: <ForgotPassword />,
-    // },
-    // {
-    //   path: "/reset-password",
-    //   element: <ResetPassword />,
-    // },
-    // {
-    //   path: "/admin",
-    //   element: <LayoutAdmin />,
-
-    //   children: [
-    //     {
-    //       path: "/admin/listfilm",
-    //       element: <ListFilm />,
-    //     },
-    //     {
-    //       path: "members",
-    //       element: <MemberInfoAdmin />,
-    //     },
-    //     {
-    //       path: "/admin/vouchers",
-    //       element: <ListVouchers />,
-    //     },
-    //     {
-    //       index: true,
-    //       element: dashbroadElement,
-    //     },
-    //     {
-    //       path: "/admin/dashboards/:cinemaId",
-    //       element: <Dashbroad_Admin_Cinema />,
-    //     },
-    //     {
-    //       path: "/admin/staff",
-    //       element: <Dashboard_Staff />,
-    //     },
-    //     {
-    //       path: "/admin/book_ticket",
-    //       element: <ListBookTicket />,
-    //     },
-    //     {
-    //       path: "/admin/listcate",
-    //       element: <ListCate />,
-    //     },
-    //     {
-    //       path: "/admin/time",
-    //       element: <ListTime />,
-    //     },
-    //     {
-    //       path: "/admin/cinema",
-    //       element: <ListCinema />,
-    //     },
-
-    //     {
-    //       path: "/admin/show",
-    //       element: <ListShow />,
-    //     },
-    //     {
-    //       path: "/admin/movieroom",
-    //       element: <ListMovieRoom />,
-    //     },
-    //     {
-    //       path: "/admin/food",
-    //       element: <ListFood />,
-    //     },
-    //     {
-    //       path: "/admin/category_detail",
-    //       element: <ListCateDetail />,
-    //     },
-    //     {
-    //       path: "/admin/user",
-    //       element: <ListUser />,
-    //     },
-    //     // {
-    //     //   path: "/admin/blogs",
-    //     //   element: <ListBlog />,
-    //     // },
-    //   ],
-    // },
-    // {
-    //   path: "/login",
-    //   element: <Login />,
-    // },
-    // {
-    //   path: "/payment/:id_code",
-    //   element: <Payment />,
-    // },
-    // {
-    //   path: "/PayMentMoMo/:id_code",
-    //   element: <PaymentMomo />,
-    // },
-    // {
-    //   path: "/ResultNapTien/:id",
-    //   element: <ResultSuccess />,
-    // },
-    // {
-    //   path: "/*",
-    //   element: <NotFound />,
-    // },
   ]);
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   const timeoutDuration = 1000 * 60 * 15; // 10 seconds
-
-  //   const timeoutCallback = async () => {
-  //     try {
-  //       // Dispatch the actions and wait for them to complete
-  //       await Promise.all([
-  //         dispatch(updateToken(null)),
-  //         dispatch(setUserId(null)),
-  //         localStorage.clear(),
-  //       ]);
-
-  //       // Once both actions are completed, navigate to the root path
-  //       window.location.href = "/";
-  //     } catch (error) {
-  //       // Handle any potential errors
-  //       console.error("Error during dispatch:", error);
-  //     }
-  //   };
-
-  //   const timeoutId = setInterval(timeoutCallback, timeoutDuration);
-
-  //   return () => {
-  //     clearInterval(timeoutId);
-  //   };
-  // }, [dispatch]);
-
   return <RouterProvider router={router} />;
 }
 

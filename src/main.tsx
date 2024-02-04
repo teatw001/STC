859d184c0e5480733f7763c631d63de90171cd91
teatw001 <duydhph26793@fpt.edu.vn>
2024-02-04 19:46:48 +0700

    up

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,14 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "./store/store.tsx";
 import { PersistGate } from "redux-persist/integration/react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import * as React from "react";
-import { Toaster } from "react-hot-toast";
-const queryClient = new QueryClient();
+// import { QueryClient, QueryClientProvider } from "react-query";
+// import * as React from "react";
+// import { Toaster } from "react-hot-toast";
+// const queryClient = new QueryClient();
 (ReactDOM as any).createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-          <Toaster />
-        </PersistGate>
-      </Provider>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );

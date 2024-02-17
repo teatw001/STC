794e3918fa8f506f7e1e5d85fe7d82baf634e7f1
teatw001 <duydhph26793@fpt.edu.vn>
2024-02-17 15:28:25 +0700

    up

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,17 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage";
+
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 import filmsAPI from "../service/films.service";
 import categorysAPI from "../service/cate.service";
-import {
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
+
 import cinemasAPI from "../service/brand.service";
 import showsAPI from "../service/show.service";
 import foodAPI from "../service/food.service";
@@ -19,7 +11,7 @@ import movieRoomAPI from "../service/movieroom.service";
 import selectedCinemaReducer from "../components/CinemaSlice/selectedCinemaSlice";
 import TKinformationReducer from "../components/CinemaSlice/selectSeat";
 import PaymentmethodReducer from "../components/ChoosePayment/ChoosePayment";
-import { combineReducers } from "redux";
+
 import timesAPI from "../service/time.service";
 import bookTicketsAPI from "../service/book_ticket.service";
 import cateDetailAPI from "../service/catedetail.service";
@@ -39,61 +31,40 @@ import commentsAPI from "../service/commentBlog.service";
 import refundAPI from "../service/refund.service";
 import commentsFilmAPI from "../service/commentfilm.service";
 
-// Import redux-persist
-const persistConfig = {
-  key: "root",
-  storage,
-  version: 1,
-  whitelist: [
-    "selectedCinema",
-    "pays",
-    "bkseats",
-    "TKinformation",
-    "Paymentmethod",
-  ],
-};
-
-const rootReducer = combineReducers({
-  films: filmsAPI.reducer,
-  cates: categorysAPI.reducer,
-  cinemas: cinemasAPI.reducer,
-  vouchers: vouchersAPI.reducer,
-  shows: showsAPI.reducer,
-  times: timesAPI.reducer,
-  pays: payAPI.reducer,
-  bookTickets: bookTicketsAPI.reducer,
-  foods: foodAPI.reducer,
-  seatKeping: seatkepingAPI.reducer,
-  movies: movieRoomAPI.reducer,
-  paymentmomo: payMoMoAPI.reducer,
-  catedetails: cateDetailAPI.reducer,
-  bkseats: bookingSeatAPI.reducer,
-  sendEmail: sendEmailAPI.reducer,
-  selectedCinema: selectedCinemaReducer,
-  TKinformation: TKinformationReducer,
-  commentsfilm: commentsFilmAPI.reducer,
-  Paymentmethod: PaymentmethodReducer,
-  users: usersAPI.reducer,
-  // blogs: blogsAPI.reducer,
-  comments: commentsAPI.reducer,
-  refund: refundAPI.reducer,
-  auth: authReducer,
-  [analyticApi.reducerPath]: analyticApi.reducer,
-  [memberAPI.reducerPath]: memberAPI.reducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => {
-    const middlewareArray = getDefaultMiddleware({
-      serializableCheck: {
-        ignoredPaths: ["selectedCinema"],
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-      immutableCheck: false,
-    }).concat(
+  reducer: {
+    // Add the generated reducer as a specific top-level slice
+    films: filmsAPI.reducer,
+    cates: categorysAPI.reducer,
+    cinemas: cinemasAPI.reducer,
+    vouchers: vouchersAPI.reducer,
+    shows: showsAPI.reducer,
+    times: timesAPI.reducer,
+    pays: payAPI.reducer,
+    bookTickets: bookTicketsAPI.reducer,
+    foods: foodAPI.reducer,
+    seatKeping: seatkepingAPI.reducer,
+    movies: movieRoomAPI.reducer,
+    paymentmomo: payMoMoAPI.reducer,
+    catedetails: cateDetailAPI.reducer,
+    bkseats: bookingSeatAPI.reducer,
+    sendEmail: sendEmailAPI.reducer,
+    selectedCinema: selectedCinemaReducer,
+    TKinformation: TKinformationReducer,
+    commentsfilm: commentsFilmAPI.reducer,
+    Paymentmethod: PaymentmethodReducer,
+    users: usersAPI.reducer,
+    // blogs: blogsAPI.reducer,
+    comments: commentsAPI.reducer,
+    refund: refundAPI.reducer,
+    auth: authReducer,
+    [analyticApi.reducerPath]: analyticApi.reducer,
+    [memberAPI.reducerPath]: memberAPI.reducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
       filmsAPI.middleware as any,
       categorysAPI.middleware as any,
       bookingSeatAPI.middleware as any,
@@ -116,16 +87,15 @@ export const store = configureStore({
       // blogsAPI.middleware as any,
       commentsAPI.middleware as any,
       refundAPI.middleware as any
-    );
-
-    return middlewareArray;
-  },
+    ),
 });
 
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
 setupListeners(store.dispatch);
 // const persistor = persistStore(store);
 
-// export { store, persistor };
+// export {  persistor };
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;

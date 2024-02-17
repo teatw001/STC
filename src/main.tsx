dc66fa744a8711c563d921c02faabaee70a987c9
teatw001 <duydhph26793@fpt.edu.vn>
2024-02-17 15:30:58 +0700

    uppp

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
-import * as ReactDOM from "react-dom";
-import App from "./App.jsx";
-import "./index.css";
+// import React from "react";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store/store.tsx";
 // import { PersistGate } from "redux-persist/integration/react";
-// import { QueryClient, QueryClientProvider } from "react-query";
-// import * as React from "react";
-// import { Toaster } from "react-hot-toast";
-// const queryClient = new QueryClient();
-(ReactDOM as any).createRoot(document.getElementById("root")!).render(
+import App from "./App.tsx";
+import "./index.css";
+import { store } from "./store/store.ts";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <App />
   </Provider>

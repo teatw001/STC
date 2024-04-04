a81def0d488e1f7c11b4beda82d78ef927964640
teatw001 <duydhph26793@fpt.edu.vn>
2024-04-04 14:50:34 +0700

    up

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 // import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.tsx";

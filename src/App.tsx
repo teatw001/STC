04c862397aae98f88f70e42918bea7ffa19c7cbb
teatw001 <duydhph26793@fpt.edu.vn>
2024-04-04 14:14:43 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routers/Router";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import LayoutUser from "./Layout/LayoutUser/LayoutUser";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Link to="/">TwittersView</Link> &nbsp;
+      <Link to="/articles">ArticlesView</Link> &nbsp;
+      <Link to="/notes">NotesView</Link>
+      <Routes>
+        <Route path="/" element={<LayoutUser />} />
+        {/* <Route path="/articles" element={<ArticlesView />} />
+        <Route path="/notes" element={<NotesView />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-function App() {
-  return <RouterProvider router={router} />;
-}
 export default App;

8038fc8c1a272598702596237719a8fef9e18a17
teatw001 <duydhph26793@fpt.edu.vn>
2024-04-04 14:02:04 +0700

    up

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import { Provider } from "react-redux";
 import App from "./App.tsx";
 import "./index.css";
 import { store } from "./store/store.ts";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );

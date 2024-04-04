8d086fbe41919cd60afabe3ac7a51f0a41501508
teatw001 <duydhph26793@fpt.edu.vn>
2024-04-04 14:35:52 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LayoutUser from "./Layout/LayoutUser/LayoutUser";
 import HomePages from "./pages/Clients/Homepages/home";
-import BookingSeat from "./pages/Clients/TICKET - SEAT LAYOUT/seat";
-import Movie_About from "./pages/Clients/MOVIE-ABOUT/Movie-About";
-import Ticket from "./pages/Clients/TICKET/Ticket";
-import Movies from "./pages/Clients/Movies/Movies";
+// import BookingSeat from "./pages/Clients/TICKET - SEAT LAYOUT/seat";
+// import Movie_About from "./pages/Clients/MOVIE-ABOUT/Movie-About";
+// import Ticket from "./pages/Clients/TICKET/Ticket";
+// import Movies from "./pages/Clients/Movies/Movies";
 // import ResultPaymentCoin from "./components/Clients/ResultPaymentCoin/ResultPaymentCoin";
-import F_B from "./pages/Clients/F&B/F&B";
-import Cinema from "./pages/Clients/Cinema/Cinema";
-import Orther from "./pages/Clients/Orther/Orther";
-import ChoosePop from "./pages/Clients/ChoosePop/ChoosePop";
+// import F_B from "./pages/Clients/F&B/F&B";
+// import Cinema from "./pages/Clients/Cinema/Cinema";
+// import Orther from "./pages/Clients/Orther/Orther";
+// import ChoosePop from "./pages/Clients/ChoosePop/ChoosePop";
 
-import TicketBookingDetails from "./pages/Clients/Ticket-booking-details/TicketBookingDetails";
+// import TicketBookingDetails from "./pages/Clients/Ticket-booking-details/TicketBookingDetails";
 const App = () => {
   return (
     <BrowserRouter>
@@ -21,11 +21,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LayoutUser />}>
           <Route path="/" element={<HomePages />} />
-          <Route path="/book-ticket/:id" element={<BookingSeat />} />
+          {/* <Route path="/book-ticket/:id" element={<BookingSeat />} />
           <Route path="/movie_about/:id" element={<Movie_About />} />
           <Route path="/ticket" element={<Ticket />} />
           <Route path="/movies" element={<Movies />} />
-          {/* <Route path="/resultpaymentcoins" element={<ResultPaymentCoin />} /> */}
+          <Route path="/resultpaymentcoins" element={<ResultPaymentCoin />} />
           <Route path="/F&B" element={<F_B />} />
           <Route path="/cinema" element={<Cinema />} />
           <Route path="/orther" element={<Orther />} />
@@ -35,7 +35,7 @@ const App = () => {
             element={<TicketBookingDetails />}
           />
           {/* <Route path="/blog/:id" element={<BlogsDetail/>} /> */}
-          <Route path="/F&B" element={<F_B />} />
+          {/* <Route path="/F&B" element={<F_B />} /> */} 
         </Route>
       </Routes>
     </BrowserRouter>
